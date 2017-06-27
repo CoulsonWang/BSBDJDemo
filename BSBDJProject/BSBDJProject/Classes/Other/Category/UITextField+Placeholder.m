19b2879f07b39df6297f73ccb6320c66241f86a2
Coulson_Wang <wangyuanyi1993@126.com>
2017-06-27 18:30:55 +0800

    优化分类。解决颜色设置顺序的问题

--- BSBDJProject/BSBDJProject/Classes/Other/Category/UITextField+Placeholder.m
+++ BSBDJProject/BSBDJProject/Classes/Other/Category/UITextField+Placeholder.m
@@ -7,16 +7,30 @@
 //
 
 #import "UITextField+Placeholder.h"
+#import <objc/message.h>
 
 @implementation UITextField (Placeholder)
 
++ (void)load {
+    Method orignalMethod = class_getInstanceMethod(self, @selector(setPlaceholder:));
+    Method customMethod = class_getInstanceMethod(self, @selector(setPlaceholder_Custom:));
+    method_exchangeImplementations(orignalMethod, customMethod);
+}
+
 - (void)setPlaceholderColor:(UIColor *)placeholderColor {
+    objc_setAssociatedObject(self, "placeholderColor", placeholderColor, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
     UILabel *placeholderLabel = [self valueForKey:@"placeholderLabel"];
     placeholderLabel.textColor = placeholderColor;
 }
 
 - (UIColor *)placeholderColor {
-    return nil;
+    return objc_getAssociatedObject(self, "placeholderColor");
+}
+
+- (void)setPlaceholder_Custom:(NSString *)placeholder {
+    [self setPlaceholder_Custom:placeholder];
+    
+    self.placeholderColor = self.placeholderColor;
 }
 
 @end
