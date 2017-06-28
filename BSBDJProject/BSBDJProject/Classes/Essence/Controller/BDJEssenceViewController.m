b496bc75d96d4cd678161e502a2fc690b1baf1ab
Coulson_Wang <wangyuanyi1993@126.com>
2017-06-28 14:21:25 +0800

    自定义按钮，重写setHighlight方法，处理按钮长按时的颜色问题

--- BSBDJProject/BSBDJProject/Classes/Essence/Controller/BDJEssenceViewController.m
+++ BSBDJProject/BSBDJProject/Classes/Essence/Controller/BDJEssenceViewController.m
@@ -8,8 +8,14 @@
 
 #import "BDJEssenceViewController.h"
 #import "UIBarButtonItem+CreateItem.h"
+#import "BDJEssenceTitleButton.h"
+
 
 @interface BDJEssenceViewController ()
+
+@property (weak, nonatomic) UIView *titlesView;
+@property (weak, nonatomic) UIScrollView *scrollView;
+@property (weak, nonatomic) BDJEssenceTitleButton *selectedTitleButton;
 
 @end
 
@@ -41,14 +47,48 @@
 
 - (void)setUpScrollView {
     UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
-    scrollView.backgroundColor = [UIColor redColor];
+    scrollView.backgroundColor = [UIColor whiteColor];
     [self.view addSubview:scrollView];
+    self.scrollView = scrollView;
 }
 
 - (void)setUpTitlesView {
     UIView *titlesView = [[UIView alloc] initWithFrame:CGRectMake(0, 64, self.view.YY_width, 44)];
     titlesView.backgroundColor = [[UIColor whiteColor] colorWithAlphaComponent:0.5];
     [self.view addSubview:titlesView];
+    self.titlesView = titlesView;
+    
+    [self setUpTitleButtons];
+    [self setUpTitleUnderline];
+}
+
+- (void)setUpTitleButtons {
+    NSArray *titles = @[@"全部",@"视频",@"声音",@"图片",@"段子"];
+    NSUInteger titleCount = titles.count;
+    
+    CGFloat titleWidth = self.titlesView.YY_width / titleCount;
+    CGFloat titleHeight = self.titlesView.YY_height;
+    
+    for (int i = 0; i < titleCount; i++) {
+        BDJEssenceTitleButton *titleButton = [BDJEssenceTitleButton buttonWithType:UIButtonTypeCustom];
+        [self.titlesView addSubview:titleButton];
+        titleButton.frame = CGRectMake(i * titleWidth, 0, titleWidth, titleHeight);
+        [titleButton setTitle:titles[i] forState:UIControlStateNormal];
+        [titleButton setTitleColor:[UIColor darkGrayColor] forState:UIControlStateNormal];
+        [titleButton setTitleColor:[UIColor redColor] forState:UIControlStateSelected];
+        
+        [titleButton addTarget:self action:@selector(titleButtonClick:) forControlEvents:UIControlEventTouchUpInside];
+    }
+}
+
+- (void)titleButtonClick:(BDJEssenceTitleButton *)btn {
+    self.selectedTitleButton.selected = NO;
+    btn.selected = YES;
+    self.selectedTitleButton = btn;
+}
+
+- (void)setUpTitleUnderline {
+    
 }
 
 /**
---
+++ BSBDJProject/BSBDJProject/Classes/Essence/View/BDJEssenceTitleButton.m
@@ -0,0 +1,17 @@
+//
+//  BDJEssenceTitleButton.m
+//  BSBDJProject
+//
+//  Created by Coulson_Wang on 2017/6/28.
+//  Copyright © 2017年 Coulson_Wang. All rights reserved.
+//
+
+#import "BDJEssenceTitleButton.h"
+
+@implementation BDJEssenceTitleButton
+
+- (void)setHighlighted:(BOOL)highlighted {
+    //覆盖掉原有的set方法，禁用高亮状态
+}
+
+@end
