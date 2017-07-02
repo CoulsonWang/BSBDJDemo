19d231b72c4d6609e61ef3f74a41760ed3b15154
Coulson_Wang <wangyuanyi1993@126.com>
2017-07-02 22:01:17 +0800

    实现保存图片到相册 1.增加请求权限时的提醒 2.实现创建或者获取自定义相册 3.把保存的图片自动关联到自定义相册

--- BSBDJProject/BSBDJProject/Classes/Essence/Controller/BDJCheckPicktureController.m
+++ BSBDJProject/BSBDJProject/Classes/Essence/Controller/BDJCheckPicktureController.m
@@ -16,6 +16,7 @@
 
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 @property (weak, nonatomic) FLAnimatedImageView *imageView;
+@property (weak, nonatomic) IBOutlet UIButton *saveButton;
 
 @end
 
@@ -24,6 +25,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self setUpImageView];
+    [SVProgressHUD setMaximumDismissTimeInterval:2.0];
     
     //实现点击返回
     [self.scrollView addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(backButtonClick:)]];
@@ -49,23 +51,39 @@
     [self.scrollView addSubview:imageView];
     self.imageView = imageView;
     
-    [imageView sd_setImageWithURL:[NSURL URLWithString:self.item.image1]];
+    [imageView sd_setImageWithURL:[NSURL URLWithString:self.item.image1] completed:^(UIImage * _Nullable image, NSError * _Nullable error, SDImageCacheType cacheType, NSURL * _Nullable imageURL) {
+        self.saveButton.enabled = YES;
+    }];
 }
 
 - (IBAction)saveButtonClick:(UIButton *)sender {
-    [[PHPhotoLibrary sharedPhotoLibrary] performChanges:^{
-        [PHAssetChangeRequest creationRequestForAssetFromImage:self.imageView.image];
-    } completionHandler:^(BOOL success, NSError * _Nullable error) {
-        if (error) {
-            [SVProgressHUD showErrorWithStatus:@"保存失败"];
-        } else {
-            [SVProgressHUD showSuccessWithStatus:@"保存成功"];
-        }
-    }];
-    [PHAssetChangeRequest creationRequestForAssetFromImage:self.imageView.image];
+    
+    //保存图片到相机胶卷
+    NSError *error = nil;
+    __block NSString *assetID = nil;
+    [[PHPhotoLibrary sharedPhotoLibrary] performChangesAndWait:^{
+        assetID = [PHAssetChangeRequest creationRequestForAssetFromImage:self.imageView.image].placeholderForCreatedAsset.localIdentifier;
+    } error:&error];
+    
+    //取得照片对象
+    PHAsset *asset = [PHAsset fetchAssetsWithLocalIdentifiers:@[assetID] options:nil].firstObject;
+    if (error) {
+        [SVProgressHUD showErrorWithStatus:@"保存失败"];
+        return;
+    } else {
+        [SVProgressHUD showSuccessWithStatus:@"保存成功"];
+        //获得自定义相册
+        PHAssetCollection *collection = [self getTheCollection];
+        //将图片与自定义相册关联起来
+        [[PHPhotoLibrary sharedPhotoLibrary] performChangesAndWait:^{
+            PHAssetCollectionChangeRequest *request = [PHAssetCollectionChangeRequest changeRequestForAssetCollection:collection];
+            [request insertAssets:@[asset] atIndexes:[NSIndexSet indexSetWithIndex:0]];
+        } error:nil];
+    }
 }
 
 - (IBAction)backButtonClick:(UIButton *)sender {
+    [SVProgressHUD dismiss];
     [self dismissViewControllerAnimated:NO completion:nil];
 }
 
@@ -74,6 +92,25 @@
 
 - (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
     return self.imageView;
+}
+
+- (PHAssetCollection *)getTheCollection {
+    //查找是否已有与项目名同名的自定义相册
+    NSString *collectionTitle = [NSBundle mainBundle].infoDictionary[@"CFBundleDisplayName"];
+    PHFetchResult *collections = [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeAlbum subtype:PHAssetCollectionSubtypeAlbumRegular options:nil];
+    for (PHAssetCollection *collection in collections) {
+        if([collection.localizedTitle isEqualToString:collectionTitle]) {
+            return collection;
+        }
+    }
+    //没有找到自定义相册
+    __block NSString *collectionID = nil;
+    //创建一个与项目名同名的相册
+    [[PHPhotoLibrary sharedPhotoLibrary] performChangesAndWait:^{
+        collectionID = [PHAssetCollectionChangeRequest creationRequestForAssetCollectionWithTitle:collectionTitle].placeholderForCreatedAssetCollection.localIdentifier;
+    } error:nil];
+    //根据唯一标识符获得刚才创建的相册
+    return [PHAssetCollection fetchAssetCollectionsWithLocalIdentifiers:@[collectionID] options:nil].firstObject;
 }
 
 @end
